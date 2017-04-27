c031b181c44118efa0cbe499f6aab8a79f19ce7b
Garuda1 <garuda1@protonmail.com>
2017-04-27 12:08:43 +0000

    Added VM memory dumping feature

--- kernel/arch/i386/self_test.c
+++ kernel/arch/i386/self_test.c
@@ -12,14 +12,20 @@
 
 void self_test(void)
 {
-  tty_puts("exp(-1) = "); tty_putn(exp(-1)); tty_putc(ASCII_NEWLINE);
-  tty_puts("exp(0)  = "); tty_putn(exp(0)); tty_putc(ASCII_NEWLINE);
-  tty_puts("exp(1)  = "); tty_putn(exp(1)); tty_putc(ASCII_NEWLINE);
-  tty_puts("0!      = "); tty_putn(fact(0)); tty_putc(ASCII_NEWLINE);
-  tty_puts("1!      = "); tty_putn(fact(1)); tty_putc(ASCII_NEWLINE);
-  tty_puts("2!      = "); tty_putn(fact(2)); tty_putc(ASCII_NEWLINE);
-  tty_puts("3!      = "); tty_putn(fact(3)); tty_putc(ASCII_NEWLINE);
-  tty_puts("4!      = "); tty_putn(fact(4)); tty_putc(ASCII_NEWLINE);
-  tty_puts("5!      = "); tty_putn(fact(5)); tty_putc(ASCII_NEWLINE);
-  tty_puts("6!      = "); tty_putn(fact(6)); tty_putc(ASCII_NEWLINE);
+  tty_puts("exp(-1)     = "); tty_putn(exp(-1)); tty_putc(ASCII_NEWLINE);
+  tty_puts("exp(0)      = "); tty_putn(exp(0)); tty_putc(ASCII_NEWLINE);
+  tty_puts("exp(1)      = "); tty_putn(exp(1)); tty_putc(ASCII_NEWLINE);
+  /*
+  tty_puts("fastexp(-1) = "); tty_putn(fastexp(-1)); tty_putc(ASCII_NEWLINE);
+  tty_puts("fastexp(0)  = "); tty_putn(fastexp(0)); tty_putc(ASCII_NEWLINE);
+  tty_puts("fastexp(1)  = "); tty_putn(fastexp(1)); tty_putc(ASCII_NEWLINE);
+  */
+  tty_puts("0!          = "); tty_putn(fact(0)); tty_putc(ASCII_NEWLINE);
+  tty_puts("1!          = "); tty_putn(fact(1)); tty_putc(ASCII_NEWLINE);
+  tty_puts("2!          = "); tty_putn(fact(2)); tty_putc(ASCII_NEWLINE);
+  tty_puts("3!          = "); tty_putn(fact(3)); tty_putc(ASCII_NEWLINE);
+  tty_puts("4!          = "); tty_putn(fact(4)); tty_putc(ASCII_NEWLINE);
+  tty_puts("5!          = "); tty_putn(fact(5)); tty_putc(ASCII_NEWLINE);
+  tty_puts("6!          = "); tty_putn(fact(6)); tty_putc(ASCII_NEWLINE);
+  tty_puts("Self test completed, make sure the different values are within an acceptable error margin\n");
 }
---
+++ kernel/arch/i386/vm/dumpmem.c
@@ -0,0 +1,39 @@
+#include <kernel/tty.h>
+#include <kernel/vm/vm.h>
+#include <libk/size_t.h>
+#include <libk/stdint.h>
+#include <ascii.h>
+#include <vga.h>
+
+static void dump_entry(const uint8_t add, const uint8_t val)
+{
+  tty_set_colour(tty_map_colour(VGA_RED, VGA_BLACK));
+  tty_putn(add);
+  tty_set_colour(tty_map_colour(VGA_LIGHT_GREY, VGA_BLACK));
+  tty_putc(':');
+  tty_set_colour(tty_map_colour(VGA_BLUE, VGA_BLACK));
+  tty_putn(val);
+  tty_set_colour(tty_map_colour(VGA_LIGHT_GREY, VGA_BLACK));
+  tty_putc(ASCII_SPACE);
+}
+
+void vm_dumpmem(const t_vm *dxvm)
+{
+  size_t i;
+  size_t j;
+
+  tty_puts("--- MEMORY DUMP ---\n");
+  i = 0;
+  while (i < VM_MEM_SIZE / 8)
+  {
+    j = 0;
+    while (j < 8 && (i * 8) + j < VM_MEM_SIZE)
+    {
+      dump_entry((i * 8) + j, dxvm->mem[(i * 8) + j]);
+      ++j;
+    }
+    tty_putc(ASCII_NEWLINE);
+    ++i;
+  }
+  tty_puts("--- MEMORY DUMP ---\n");
+}
